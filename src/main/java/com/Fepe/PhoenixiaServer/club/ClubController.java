bcf75a42daaf26691512190d1d28fa72e14cbfe4
wwan13 <wwan13@naver.com>
2023-05-05 22:18:13 +0900

    [feat]: club controller

--- src/main/java/com/Fepe/PhoenixiaServer/club/ClubController.java
+++ src/main/java/com/Fepe/PhoenixiaServer/club/ClubController.java
@@ -1,2 +1,44 @@
-package com.Fepe.PhoenixiaServer.club;public class ClubController {
+package com.Fepe.PhoenixiaServer.club;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/club")
+public class ClubController {
+
+    private final ClubService clubService;
+
+    @GetMapping
+    public ResponseEntity queryAllClubs() {
+        List<Club> clubs = this.clubService.queryAllClubs();
+        return ResponseEntity.ok().body(clubs);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity findClub(@PathVariable Integer id) {
+        Club club = this.clubService.findClubById(id);
+        return ResponseEntity.ok().body(club);
+    }
+
+    @PostMapping
+    public ResponseEntity createClub(@RequestBody ClubDto clubDto) {
+        Club club = this.clubService.createClub(clubDto);
+        URI createdUri = linkTo(methodOn(ClubController.class).createClub(clubDto)).slash(club).toUri();
+        return ResponseEntity.created(createdUri).body(club);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity deleteClub(@PathVariable Integer id) {
+        this.clubService.deleteClub(id);
+        return ResponseEntity.ok().body(id);
+    }
 }
---
+++ src/test/java/com/Fepe/PhoenixiaServer/foodtruck/FoodTruckControllerTest.java
@@ -0,0 +1,12 @@
+package com.Fepe.PhoenixiaServer.foodtruck;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FoodTruckControllerTest {
+
+}
