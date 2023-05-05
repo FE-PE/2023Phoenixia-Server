f0b180c1fe7a276569cda887745ba1e841e1118b
wwan13 <wwan13@naver.com>
2023-05-05 22:49:02 +0900

    [feat]: swagger api

--- src/test/java/com/Fepe/PhoenixiaServer/foodtruck/FoodTruckControllerTest.java
+++ src/test/java/com/Fepe/PhoenixiaServer/foodtruck/FoodTruckControllerTest.java
@@ -1,12 +1,15 @@
 package com.Fepe.PhoenixiaServer.foodtruck;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 class FoodTruckControllerTest {
 
 }
