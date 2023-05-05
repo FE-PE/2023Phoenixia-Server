4a523fa0f411025398d8e85f0bfe1b30f5b535d0
wwan13 <wwan13@naver.com>
2023-05-05 20:48:38 +0900

    [feat]: init project

---
+++ src/main/java/com/Fepe/PhoenixiaServer/Application.java
@@ -0,0 +1,13 @@
+package com.Fepe.PhoenixiaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
