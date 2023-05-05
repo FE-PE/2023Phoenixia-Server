e0d62eb4e7ef570fa2952da7e9d13ae015fe1731
wwan13 <wwan13@naver.com>
2023-05-05 22:50:48 +0900

    [fix]: swagger config base package

--- src/main/java/com/Fepe/PhoenixiaServer/common/SwaggerConfig.java
+++ src/main/java/com/Fepe/PhoenixiaServer/common/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.club"))
+                .apis(RequestHandlerSelectors.basePackage("com.Fepe.PhoenixiaServer"))
                 .paths(PathSelectors.any())
                 .build();
     }
