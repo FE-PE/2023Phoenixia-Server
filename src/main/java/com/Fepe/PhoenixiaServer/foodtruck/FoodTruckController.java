6fccf666497fbcc5e3ad454ae4220ed0d29f0d3f
wwan13 <wwan13@naver.com>
2023-05-05 21:29:02 +0900

    [feat]: food truck controller

---
+++ src/main/java/com/Fepe/PhoenixiaServer/foodtruck/FoodTruckController.java
@@ -0,0 +1,45 @@
+package com.Fepe.PhoenixiaServer.foodtruck;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/foodtruck")
+public class FoodTruckController {
+
+    private final FoodTruckService foodTruckService;
+
+    @GetMapping
+    public ResponseEntity queryAllFoodTrucks() {
+        List<FoodTruck> foodTrucks = this.foodTruckService.queryAllFoodTrucks();
+        return ResponseEntity.ok().body(foodTrucks);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity getFoodTruck(@PathVariable Integer id) {
+        FoodTruck foodTruck = this.foodTruckService.findFoodTruckById(id);
+        return ResponseEntity.ok().body(foodTruck);
+    }
+
+    @PostMapping
+    public ResponseEntity createFoodTruck(@RequestBody FoodTruckDto foodTruckDto) {
+        FoodTruck foodTruck = this.foodTruckService.createFoodTruck(foodTruckDto);
+        URI createdUri = linkTo(methodOn(FoodTruckController.class).createFoodTruck(foodTruckDto)).slash(foodTruck).toUri();
+
+        return ResponseEntity.created(createdUri).body(foodTruck);
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteFoodTruck(@PathVariable Integer id) {
+        this.foodTruckService.deleteFoodTruckById(id);
+        return ResponseEntity.ok().body(id);
+    }
+}
